891ff5113144f9d8eb9759a9e3f4c277a8bff868
sonicmy <9163540822n@gmail.com>
2013-09-19 18:16:55 +0400

    Solution Task 3

---
+++ src/main/scala/3.scala
@@ -0,0 +1,20 @@
+#! /bin/sh
+  exec scala "$0" "$@"
+!#
+
+def primes(n:Long):Array[Long] = {
+  val sqrt = math.round(math.sqrt(n))
+  
+  def div(iter:Double):(Long,Long) = {
+    if(iter == sqrt) n -> n
+    else if(n%iter==0) (n/iter).toLong -> iter.toLong
+    else div(iter+1)
+  }
+
+  val (prime, iter) = div(2)
+  if(prime==n) Array(n)
+  else iter +: primes(prime)
+}
+
+val num = 600851475143L
+primes(num).foreach(println)
