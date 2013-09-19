dccc89fc802734f68694b29aa0367fd0a8570e02
sonicmy <9163540822n@gmail.com>
2013-09-19 18:51:28 +0400

    Solution Task 4

---
+++ src/main/scala/4.scala
@@ -0,0 +1,6 @@
+#! /bin/sh
+  exec scala "$0" "$@"
+!#
+// The solution is too straightforvard but digits are too small to think hard.
+println((for( i <- 999 to 1 by -1; j <- 999 to 1 by -1; if((j*i).toString == (i*j).toString.reverse) ) yield (i*j)).toArray.max)
+
